37c82fdd889c1846231d93382ffc203d59b44998
yeyushengfan258 <258977548@qq.com>
2022-09-18 01:15:49 +0800

    update

--- install.sh
+++ install.sh
@@ -13,7 +13,7 @@ fi
 SRC_DIR=$(cd $(dirname $0) && pwd)
 
 THEME_NAME=Win11
-THEME_VARIANTS=('-black' '-blue' '-red' '-purple' '-green')
+THEME_VARIANTS=('-black' '-blue' '-red' '-nord' '-purple' '-green')
 COLOR_VARIANTS=('' '-dark')
 
 usage() {
